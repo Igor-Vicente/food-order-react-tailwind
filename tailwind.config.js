cd1dbe5ed252d0f27f6b94dae85ea54bc00a585e
igor vicente <igor_machado05@hotmail.com>
2024-04-23 07:29:52 -0300

    added backend server simulation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgBodyGradient1: '#29251c',
+        bgBodyGradient2: '#2c2306',
+        dimWhite: 'rgba(255, 255, 255, 0.7)',
+        dimBlue: 'rgba(9, 151, 124, 0.1)',
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
